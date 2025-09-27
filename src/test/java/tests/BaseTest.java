59dbbe777b289325e1267c88c59a9cf9c5b52538
QA academy <education@qaacademy.rs>
2025-09-27 17:43:31 +0200

    Fixes2

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,9 +2,13 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 import pages.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 public class BaseTest {
@@ -16,24 +20,31 @@ public class BaseTest {
     protected CheckOutOverviewPage checkOutOverviewPage;
     protected CheckOutUsersInfoPage checkOutUsersInfoPage;
     protected CartPage cartPage;
+    private Path tmpProfileDir;
 
     @BeforeClass
-    public void beforeClass() {
+    public void beforeClass() throws IOException {
+        // Jedinstveni, čisti Chrome profil za ovaj test class
+        tmpProfileDir = Files.createTempDirectory("wd-profile-");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=" + tmpProfileDir.toAbsolutePath());
+        options.addArguments("--no-first-run", "--no-default-browser-check");
+        // (po želji za CI/Linux) options.addArguments("--disable-dev-shm-usage", "--no-sandbox");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver.get("https://www.saucedemo.com/");
         logInPage = new LogInPage(driver);
         homePage = new HomePage(driver);
         checkOutCompletePage = new CheckOutCompletePage(driver);
         checkOutOverviewPage = new CheckOutOverviewPage(driver);
         checkOutUsersInfoPage = new CheckOutUsersInfoPage(driver);
         cartPage = new CartPage(driver);
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        driver.get("https://www.saucedemo.com/");
     }
 
     @AfterMethod
@@ -43,7 +54,18 @@ public class BaseTest {
 
     @AfterClass
     public void afterClass() {
-        driver.quit();
+        // Zatvori driver
+        if (driver != null) {
+            try { driver.quit(); } catch (Exception ignored) {}
+        }
+        // Očisti privremeni profil (uklanja eventualni lock)
+        if (tmpProfileDir != null) {
+            try {
+                Files.walk(tmpProfileDir)
+                        .sorted((a, b) -> b.compareTo(a))
+                        .forEach(p -> { try { Files.deleteIfExists(p); } catch (Exception ignored) {} });
+            } catch (Exception ignored) {}
+        }
     }
 }
 

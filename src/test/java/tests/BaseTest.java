b21c0f200570030f7e9ddbb36576aef9ff6bec70
QA academy <education@qaacademy.rs>
2025-09-27 17:28:29 +0200

    Fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,27 +1,34 @@
 package tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
-import pages.HomePage;
-import pages.LogInPage;
+import pages.*;
 
 import java.time.Duration;
 
 public class BaseTest {
 
-    protected static WebDriver driver;
+    protected WebDriver driver;
+    protected LogInPage logInPage;
+    protected HomePage homePage;
+    protected CheckOutCompletePage checkOutCompletePage;
+    protected CheckOutOverviewPage checkOutOverviewPage;
+    protected CheckOutUsersInfoPage checkOutUsersInfoPage;
+    protected CartPage cartPage;
 
     @BeforeClass
     public void beforeClass() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-//        max time to wait for a page load
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
-//        max time for asynchronous JS run
+        logInPage = new LogInPage(driver);
+        homePage = new HomePage(driver);
+        checkOutCompletePage = new CheckOutCompletePage(driver);
+        checkOutOverviewPage = new CheckOutOverviewPage(driver);
+        checkOutUsersInfoPage = new CheckOutUsersInfoPage(driver);
+        cartPage = new CartPage(driver);
     }
 
     @BeforeMethod
--- src/test/java/tests/LogInTests.java
+++ src/test/java/tests/LogInTests.java
@@ -5,13 +5,6 @@ import pages.*;
 
 public class LogInTests extends BaseTest {
 
-    LogInPage logInPage = new LogInPage(driver);
-    HomePage homePage = new HomePage(driver);
-    CheckOutCompletePage checkOutCompletePage = new CheckOutCompletePage(driver);
-    CheckOutOverviewPage checkOutOverviewPage = new CheckOutOverviewPage(driver);
-    CheckOutUsersInfoPage checkOutUsersInfoPage = new CheckOutUsersInfoPage(driver);
-    CartPage cartPage = new CartPage(driver);
-
 //    @Test
 //    public void validLogInCredentials() {
 //        logInPage.validLogInAction();

a944f2aefa7975d08976be5542494673560508f6
QA academy <education@qaacademy.rs>
2026-07-11 15:34:35 +0200

    New code

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -8,7 +8,12 @@ public class HomePage extends BasePage {
     private By addToCartButton = By.name("add-to-cart-sauce-labs-backpack");
     private By shopingCartIcon = By.id("shopping_cart_container");
     private By locator = By.id("locator");
-    
+    private By locatorNumberTwo = By.id("something");
+    private By locatorx = By.id("locator");
+    private By locatorNumberTwoxx = By.id("something");
+    private By locatortt = By.id("locator");
+    private By locatorNumberTwoxq = By.id("something");
+
     public HomePage(WebDriver driver) {
         super(driver);
     }
